98318e50fce2d7e8292d6d531bef333738fd3dc3
stacietaylorcima <stacietaylorcima@gmail.com>
2017-10-03 10:00:25 -0700

    create basic to-do app with task creation funtionality

---
+++ app.js
@@ -0,0 +1,22 @@
+function onReady() {
+   const addToDoForm = document.getElementById('addToDoForm');
+   const newToDoText = document.getElementById('newToDoText');
+   const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', (event) => {
+     event.preventDefault();
+     let title = newToDoText.value;
+     let newLi = document.createElement('li');
+     let checkbox = document.createElement('input');
+     checkbox.type = "checkbox";
+     newLi.textContent = title;
+     toDoList.appendChild(newLi);
+     newLi.appendChild(checkbox);
+     newToDoText.value = '';
+   });
+ }
+
+window.onload = function() {
+  alert("The window has loaded!");
+  onReady();
+};
