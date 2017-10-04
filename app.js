ce63d9bc187d1e0a6d37146f48dc1c096f1fca50
stacietaylorcima <stacietaylorcima@gmail.com>
2017-10-04 14:32:47 -0700

    refactor code to seperate state and UI

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
 function onReady() {
-   const addToDoForm = document.getElementById('addToDoForm');
-   const newToDoText = document.getElementById('newToDoText');
-   const toDoList = document.getElementById('toDoList');
+  var toDos = [];
+  var addToDoForm = document.getElementById('addToDoForm');
 
-  addToDoForm.addEventListener('submit', (event) => {
-     event.preventDefault();
-     let title = newToDoText.value;
-     let newLi = document.createElement('li');
-     let checkbox = document.createElement('input');
-     let deleteButton = document.createElement('input')
-     deleteButton.className = 'dltbtn';
-     deleteButton.type = 'button';
-     deleteButton.name = 'delbtn';
-     deleteButton.value = "Delete";
-     checkbox.type = "checkbox";
-     newLi.textContent = title;
-     toDoList.appendChild(newLi);
-     newLi.appendChild(checkbox);
-     newLi.appendChild(deleteButton);
-     newToDoText.value = '';
 
-   deleteButton.addEventListener('click', (event) => {
-     event.preventDefault();
-     toDoList.removeChild(newLi);
-   });
-      });
+  function createNewToDo() {
+    var newToDoText = document.getElementById('newToDoText');
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+
+    newToDoText.value = '';
+
+    renderTheUI(toDos);
+  }
+
+  function renderTheUI(toDos) {
+    var toDoList = document.getElementById('toDoList');
+
+    toDoList.innerHTML = '';
+
+    toDos.forEach(function(toDo) {
+      var newLi = document.createElement('li');
+      var checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.innerHTML = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+
+  addToDoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    createNewToDo();
+  });
+
+  renderTheUI(toDos);
+
  }
 
-
 window.onload = function() {
-  alert("The window has loaded!");
   onReady();
 };
